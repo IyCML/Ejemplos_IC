25f8e728c42a97287a1eddc2fe9f62db10dc7622
LeslieLesC <42645860+LeslieLesC@users.noreply.github.com>
2018-09-26 00:03:19 -0300

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,25 @@
+def yield_chunks(arr, chunk_size):
+    larr = len(arr)
+    if larr < chunk_size:
+        raise ValueError("The array length (%d) must be larger than the chunk size (%d)" % (len(arr), chunk_size))
+
+    cursor = 0
+    while cursor < larr:
+        next_cursor = min(cursor + chunk_size, larr)
+        yield arr[cursor:next_cursor]
+        cursor = next_cursor
+
+
+def yield_chunks_forever(arr, chunk_size):
+    while True:
+        yield from yield_chunks(arr, chunk_size)
+
+
+def yield_chunks_repeat(arr, chunk_size, times):
+    for n in range(times):
+        yield from yield_chunks(arr, chunk_size)
+
+
+x = list(range(10))
+print(list(yield_chunks(x, 3)))
+print(list(yield_chunks(x, 2)))
